c099a4d3537c53663276c5f39ad1c368d4520e50
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-06-23 11:27:53 -0300

    updated models; merget it in a single file

---
+++ api/models.ts
@@ -0,0 +1,95 @@
+export interface ProfileSearchModel {
+    id: string;
+    handle: string;
+    university: UniversitySearchModel;
+}
+
+export interface UniversitySearchModel {
+    id: string;
+    name: string;
+    location: string;
+}
+
+export interface TeamSearchModel {
+    id: string;
+    name: string;
+    members: ProfileSearchModel[];
+    univesity?: UniversitySearchModel;
+}
+
+export interface ContestSearchModel {
+    id: string;
+    name: string;
+    year: string;
+}
+
+export interface EventSearchModel {
+    id: string;
+    name: string;
+    location: string;
+}
+
+export interface ContestPerformanceModel {
+    position: number;
+    contest: ContestSearchModel;
+}
+
+export interface ProfileFullModel {
+    id: string;
+    handle: string;
+    university?: UniversitySearchModel;
+    teams: TeamSearchModel[];
+    events: EventSearchModel[];
+    contests: ContestPerformanceModel[];
+}
+
+export interface UniversityFullModel {
+    id: string;
+    name: string;
+    location: string;
+    students: ProfileSearchModel[];
+    teams: TeamSearchModel[];
+    contests: ContestSearchModel[];
+}
+
+export interface TeamFullModel {
+    id: string;
+    name: string;
+    university?: UniversitySearchModel;
+    members: ProfileSearchModel[];
+    contests: ContestPerformanceModel[];
+}
+
+export interface ContestFullModel {
+    id: string;
+    name: string;
+    year: string;
+    officialPageUrl: string;
+    problemsPdfUrl: string; // TODO: Remover Url
+    solutionsPdfUrl: string; // TODO: Remover Url
+    ranking: TeamResultModel[];
+}
+
+export interface TeamResultModel {
+    team: TeamSearchModel; // maybe TeamSearchModel | ProfileSearchModel 
+    position: number;
+    penalty: number;
+    submissions: SubmissionModel[];
+}
+
+export interface SubmissionModel {
+    problemId: string;
+    success: boolean;
+    tries: number;
+    penalty: number;
+}
+
+export interface ProblemSearchModel {
+    id: string;
+    name: string;
+    label: string;
+}
+
+export interface ProblemFullModel extends ProblemSearchModel { // TODO: criar pagina inteira
+    setter: ProfileSearchModel;
+}
